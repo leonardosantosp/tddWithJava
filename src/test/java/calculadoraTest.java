00b626fd4bb91a9823bb82781cccc525170c18f4
leonardosantossp <leo040604@gmail.com>
2023-09-26 12:59:32 -0300

    commit calculator

--- src/test/java/calculadoraTest.java
+++ src/test/java/calculadoraTest.java
@@ -11,7 +11,7 @@ public class calculadoraTest {
     }
 
     @Test
-    @DisplayName("add > when both number are positive > return a positive number")
+    @DisplayName("Add > when both number are positive > return a positive number")
     void addWhenBothNumbersArePositiveReturnAPositiveNumber(){
         int result = calculator.add(3,4);
         //se result == 7
@@ -19,24 +19,28 @@ public class calculadoraTest {
     }
 
     @Test
+    @DisplayName("Add > when both number are negative > return a negative number")
     void addWhenBothNumbersAreNegativeReturnANegativeNumber(){
         int result = calculator.add(-3,-4);
         Assertions.assertEquals(-7,result);
     }
 
     @Test
+    @DisplayName("Add > when one number is positive and the another is negative > return a positive number")
     void addWhenOneNumberIsPositiveAndTheAnotherIsNegativeReturnAPositiveNumber(){
         int result = calculator.add(-3,4);
         Assertions.assertEquals(1,result);
     }
 
     @Test
+    @DisplayName("Add > when one number is positive and the another is negative > return a negative number")
     void addWhenOneNumberIsNegativeAndTheAnotherIsPositiveReturnANegativeNumber(){
         int result = calculator.add(3,-4);
         Assertions.assertEquals(-1,result);
     }
 
     @Test
+    @DisplayName("Add > when one number is zero > return the given number")
     void addWhenOneNumberIsZeroReturnTheGivenNumber(){
         int result = calculator.add(3,0);
         int secondResult = calculator.add(0,4);
@@ -47,7 +51,8 @@ public class calculadoraTest {
     }
 
     @Test
-    void addWhenOneNumberIsNegativeAndTheAnotherIsZeroReturnTheGIvenNumber(){
+    @DisplayName("Add > when one number is negative and the another is zero > return the given number")
+    void addWhenOneNumberIsNegativeAndTheAnotherIsZeroReturnTheGivenNumber(){
         int result = calculator.add(-3,0);
         int secondResult = calculator.add(0,-4);
         Assertions.assertAll(
@@ -57,6 +62,7 @@ public class calculadoraTest {
     }
 
     @Test
+    @DisplayName("Add > when both numbers are zero > return zero")
     void addWhenBothNumbersAreZeroReturnZero(){
         int result = calculator.add(0,0);
         Assertions.assertEquals(0,result);
@@ -64,6 +70,14 @@ public class calculadoraTest {
     @Test
     @DisplayName("#Divide > When the divider is zero > Throw an exception")
     void divideWhenTheDividerIzZeroThrowAnException(){
+        Assertions.assertThrows(ArithmeticException.class, ()-> {
+            calculator.divide(5,0);
+        });
+    }
+
+    @Test
+    @DisplayName("#Divide > When both numbers are positive >  return a int number")
+    void divideWhenBothNumbersArePositiveReturnAIntNumber(){
         Assertions.assertThrows(ArithmeticException.class, ()-> {
             calculator.divide(5,0);
         });
